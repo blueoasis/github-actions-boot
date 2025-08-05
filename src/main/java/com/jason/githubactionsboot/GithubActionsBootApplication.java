aa5bdb5bd396d5d4cd7240cd6f0a7bba866a0d8f
blueoasis <blueoasis@users.noreply.github.com>
2025-08-04 23:00:13 -0600

    initial commit

---
+++ src/main/java/com/jason/githubactionsboot/GithubActionsBootApplication.java
@@ -0,0 +1,13 @@
+package com.jason.githubactionsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jason/githubactionsboot/GithubActionsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.githubactionsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
